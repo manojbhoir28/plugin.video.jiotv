b63bbcbd19e89dce3f414526d61bf02ccb278a6c
Manoj <manojbhoir28@gmail.com>
2023-01-22 14:43:06 +0530

    Fixed channels not working problem

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -184,14 +184,3 @@ def _install_addon(addonid):
         Script.log('{addon} add-on not installed.'.format(addon=addonid))
         return False
 
-def quality_to_enum(quality_str):
-    """Converts quality into a numeric value. Ensure to check for bounds before using."""
-    mapping = {
-        'Best': 3,
-        'Low': 0,
-        'Medium': 1,
-        'High': 2
-    }
-    if quality_str in mapping:
-        return mapping[quality_str]
-    return 0
